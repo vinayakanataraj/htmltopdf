17f89e3981a4489e00009730f727a5db80519976
vinayakanataraj <86285610+vinayakanataraj@users.noreply.github.com>
2024-02-14 23:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import pdfkit
+import base64
+
+def convert_pdf_to_base64(pdf_file_path):
+    """Converts a PDF file to base64 format, suitable for printer input.
+
+    Args:
+        pdf_file_path (str): The path to the PDF file.
+
+    Returns:
+        str: The base64-encoded representation of the PDF file.
+    """
+
+    with open(pdf_file_path, "rb") as pdf_file:
+        pdf_data = pdf_file.read()
+        encoded_data = base64.b64encode(pdf_data).decode("utf-8")  # Encode and decode for string output
+
+    return encoded_data
 
 def convert_html_to_pdf(html_file, pdf_file):
     options = {
